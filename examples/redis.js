bd29aa4b409ad5bc4dc923ec239e57887e69e78f
Adam Rudd <adam.rudd@uqconnect.edu.au>
2012-01-11 16:43:34 +1000

    Added redis server example

---
+++ examples/redis.js
@@ -0,0 +1,39 @@
+var mqtt = require('../mqtt'),
+	redis = require('redis');
+
+mqtt.createServer(function(client) {
+	client.p = redis.createClient(null, null, {no_ready_check: true});
+	client.s = redis.createClient(null, null, {no_ready_check: true});
+
+	client.on('connect', function(packet) {
+		client.connack({returnCode: 0});
+	});
+
+	client.on('subscribe', function(packet) {
+		var granted = [];
+		for (var i = 0; i < packet.subscriptions.length; i++) {
+			var sub = packet.subscriptions[i]
+			granted.push(sub.qos);
+
+			client.s.psubscribe(sub.topic
+								  .replace(/\+/g, '[^/]')
+								  .replace(/\#/g, '*'));
+		}
+
+		client.suback({messageId: packet.messageId, granted: granted});
+	});
+	client.on('publish', function(packet) {
+		client.p.publish(packet.topic, packet.payload);
+	});
+	client.on('pingreq', function(packet) {
+		client.pingresp();
+	});
+	client.on('close', function() {
+		client.p.end();
+		client.s.end();
+	});
+	client.s.on('pmessage', function(pattern, channel, message) {
+		client.publish({topic: channel, payload: message});
+	});
+}).listen(1883);
+
