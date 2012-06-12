eadf845f05b4199222f1bed9b29db4f465171ace
Adam Rudd <adam.rudd@uqconnect.edu.au>
2012-06-13 09:53:40 +1000

    Retabbed test files

--- tests/parse.js
+++ tests/parse.js
@@ -1,65 +1,65 @@
 var fs = require('fs'),
-	assert = require('assert'),
-	util = require('util'),
-	EventEmitter = require('events').EventEmitter,
-	Connection = require('../lib/connection');
+  assert = require('assert'),
+  util = require('util'),
+  EventEmitter = require('events').EventEmitter,
+  Connection = require('../lib/connection');
 
 var test = module.exports.test = function() {
 
-	var cases = JSON.parse(fs.readFileSync('./parse.json', 'utf8'));
+  var cases = JSON.parse(fs.readFileSync('./parse.json', 'utf8'));
 
-	var tester = new EventEmitter();
+  var tester = new EventEmitter();
 
-	tester.write = function(tester) {
-		return function(data) {
-			tester.emit('data', data);
-		}
-	}(tester);
+  tester.write = function(tester) {
+    return function(data) {
+      tester.emit('data', data);
+    }
+  }(tester);
 
-	console.log('\n\nTesting packet parsing\n\n');
+  console.log('\n\nTesting packet parsing\n\n');
 
-	var uut = new Connection(tester);
+  var uut = new Connection(tester);
 
-	for(type in cases) {
-		var suite = cases[type];
+  for(type in cases) {
+    var suite = cases[type];
 
-		console.log('Testing %s packet parsing', type);
+    console.log('Testing %s packet parsing', type);
 
-		for (var i = 0; i < suite.length; i++) {
-			var test = suite[i],
-				desc = test.description,
-				expected = test.expected,
-				input = new Buffer(test.fixture);
+    for (var i = 0; i < suite.length; i++) {
+      var test = suite[i],
+        desc = test.description,
+        expected = test.expected,
+        input = new Buffer(test.fixture);
 
-			console.log('\tTesting %s', desc);
+      console.log('\tTesting %s', desc);
 
-			uut.once(type, function(expected) {
-				return function(packet) {
-					var wasError = false;
+      uut.once(type, function(expected) {
+        return function(packet) {
+          var wasError = false;
 
-					for (var k in expected) {
-						try {
-							assert.ok(k in packet, {k: k});
-							assert.deepEqual(packet[k], expected[k], {k: k, ev: expected[k],
-										 av: packet[k]});
-						} catch(e) {
-							var e = e.message;
-							wasError = true;
-							if ('ev' in e) {
-								console.log('\t\tPacket[%s]: %j\n\t\tExpected[%s]: %j',
-											e.k, e.av, e.k, e.ev);
-							} else if('k' in e) {
-								console.log('\t\tKey %s not present in parsed packet', e.k);
-							}
-						}
-					}
+          for (var k in expected) {
+            try {
+              assert.ok(k in packet, {k: k});
+              assert.deepEqual(packet[k], expected[k], {k: k, ev: expected[k],
+                     av: packet[k]});
+            } catch(e) {
+              var e = e.message;
+              wasError = true;
+              if ('ev' in e) {
+                console.log('\t\tPacket[%s]: %j\n\t\tExpected[%s]: %j',
+                      e.k, e.av, e.k, e.ev);
+              } else if('k' in e) {
+                console.log('\t\tKey %s not present in parsed packet', e.k);
+              }
+            }
+          }
 
-					if (!wasError) console.log('\t\tTest passed') ;
+          if (!wasError) console.log('\t\tTest passed') ;
 
-				}
-			}(expected));
+        }
+      }(expected));
 
-			tester.write(input);
-		}
-	}
+      tester.write(input);
+    }
+  }
 }
