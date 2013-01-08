f85f799cd43ade6108fc6f80fc90190e5bcb0475
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-01-08 16:32:04 +0100

    Moved tls and evict examples to appropriate dirs

--- examples/client/tls-client.js
+++ examples/client/tls-client.js
@@ -1,6 +1,6 @@
 var mqtt = require('../lib/mqtt');
 
-mqtt.createSecureClient(1883, "127.0.0.1", "private-key.pem", "public-cert.pem", function(err,client) {
+mqtt.createSecureClient(process.argv[2], process.argv[3], "private-key.pem", "public-cert.pem", function(err,client) {
 	if ( client ) {
 		console.log("Client isn't null");
 		client.connect({ keepalive: 60, client: "tls_test_client" });
--- examples/server/tls.js
+++ examples/server/tls.js
@@ -44,4 +44,4 @@ mqtt.createSecureServer("private-key.pem", "public-cert.pem", function(client) {
 		client.stream.end();
 		util.log('error!');
 	});
-}).listen(1883);
+}).listen(process.argv[2]);
