46a284c9c4cee22a08424bd64df93e7235303349
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-01-09 12:31:14 +0100

    Retabbed tls examples

--- examples/client/tls-client.js
+++ examples/client/tls-client.js
@@ -1,13 +1,13 @@
-var mqtt = require('../lib/mqtt');
+var mqtt = require('../..');
 
 mqtt.createSecureClient(process.argv[2], process.argv[3], "private-key.pem", "public-cert.pem", function(err,client) {
-	if ( client ) {
-		console.log("Client isn't null");
-		client.connect({ keepalive: 60, client: "tls_test_client" });
-		client.on("connack", function(packet) {
-			console.log( JSON.stringify(packet) );
-		});
-	} else {
-		console.log("Error " + JSON.stringify(err));
-	}
+  if (client) {
+    console.log("Client isn't null");
+    client.connect({ keepalive: 60, client: "tls_test_client" });
+    client.on("connack", function(packet) {
+      console.log( JSON.stringify(packet) );
+    });
+  } else {
+    console.log("Error " + JSON.stringify(err));
+  }
 });
--- examples/server/tls.js
+++ examples/server/tls.js
@@ -3,45 +3,45 @@
 var mqtt = require('../lib/mqtt');
 
 mqtt.createSecureServer("private-key.pem", "public-cert.pem", function(client) {
-	var self = this;
+  var self = this;
 
-	if (!self.clients) self.clients = {};
+  if (!self.clients) self.clients = {};
 
-	client.on('connect', function(packet) {
-		client.connack({returnCode: 0});
-		client.id = packet.client;
-		self.clients[client.id] = client;
-	});
+  client.on('connect', function(packet) {
+    client.connack({returnCode: 0});
+    client.id = packet.client;
+    self.clients[client.id] = client;
+  });
 
-	client.on('publish', function(packet) {
-		for (var k in self.clients) {
-			self.clients[k].publish({topic: packet.topic, payload: packet.payload});
-		}
-	});
+  client.on('publish', function(packet) {
+    for (var k in self.clients) {
+      self.clients[k].publish({topic: packet.topic, payload: packet.payload});
+    }
+  });
 
-	client.on('subscribe', function(packet) {
-		var granted = [];
-		for (var i = 0; i < packet.subscriptions.length; i++) {
-			granted.push(packet.subscriptions[i].qos);
-		}
+  client.on('subscribe', function(packet) {
+    var granted = [];
+    for (var i = 0; i < packet.subscriptions.length; i++) {
+      granted.push(packet.subscriptions[i].qos);
+    }
 
-		client.suback({granted: granted});
-	});
+    client.suback({granted: granted});
+  });
 
-	client.on('pingreq', function(packet) {
-		client.pingresp();
-	});
+  client.on('pingreq', function(packet) {
+    client.pingresp();
+  });
 
-	client.on('disconnect', function(packet) {
-		client.stream.end();
-	});
+  client.on('disconnect', function(packet) {
+    client.stream.end();
+  });
 
-	client.on('close', function(err) {
-		delete self.clients[client.id];
-	});
+  client.on('close', function(err) {
+    delete self.clients[client.id];
+  });
 
-	client.on('error', function(err) {
-		client.stream.end();
-		util.log('error!');
-	});
+  client.on('error', function(err) {
+    client.stream.end();
+    util.log('error!');
+  });
 }).listen(process.argv[2]);
