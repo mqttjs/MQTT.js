b3e0db3a8edddd030c8442824cc9d271bf45e580
Peter Magnusson <peter@birchroad.net>
2013-01-25 07:34:37 +0100

    Added some more tests.
    Mainly for lib/connection and lib/mqtt

--- test/tester.js
+++ test/tester.js
@@ -1,14 +1,34 @@
-var EventEmitter = require('events').EventEmitter;
+var EventEmitter = require('events').EventEmitter,
+  util = require('util'),
+  assert = require('assert');
 
 /*
  * Generates a mock stream object used for testing
  */
-module.exports = function () {
-  var tester = new EventEmitter();
-  tester.write = (function (tester) {
-    return function (data) {
-      tester.emit('data', data);
-    };
-  }(tester));
-  return tester;
+var Tester = function () {
+  this.open = true;
+  EventEmitter.call(this);
 };
+
+util.inherits(Tester, EventEmitter);
+
+Tester.prototype.write = function (data) {
+  assert.ok(this.open, "Not allowed to write, stream is closed");
+  this.emit('data', data);
+};
+
+Tester.prototype.end = function () {
+  this.open = false;
+};
+
+/*
+ * used to fake events like close and error in tests
+ */
+Tester.prototype.do_emit = function (evt, data) {
+  this.emit(evt, data);
+};
+
+module.exports = function () {
+  return new Tester();
+};
+
