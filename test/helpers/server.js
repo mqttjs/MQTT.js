157969a6bbdf4d9ba9c4042b1a8fccc35aa05eaa
Peter Magnusson <peter@birchroad.net>
2013-01-25 10:45:14 +0100

    Added tests for QOS.
    Inspired by comments in issue #18

---
+++ test/helpers/server.js
@@ -0,0 +1,48 @@
+var mqtt = require('../../lib/mqtt');
+
+module.exports.init_server = function (PORT) {
+  var server = mqtt.createServer(function (client) {
+    /*var i, events = ['connect', 'publish', 'pubrel', 'subscribe', 'disconnect'];
+
+    for (i = 0; i < events.length; i++) {
+      client.on(events[i], function (packet) {
+        //console.dir(packet);
+      });
+    }
+    */
+
+    client.on('connect', function (packet) {
+      client.connack(0);
+    });
+
+    client.on('publish', function (packet) {
+      switch (packet.qos) {
+      case 1:
+        client.puback({messageId: packet.messageId});
+        break;
+      case 2:
+        client.pubrec({messageId: packet.messageId});
+        break;
+      default:
+        //console.log('errors? QOS=', packet.qos);
+        break;
+      }
+
+    });
+
+    client.on('pubrel', function (packet) {
+      client.pubcomp({messageId: packet.messageId});
+    });
+
+    client.on('pingreq', function (packet) {
+      client.pingresp();
+    });
+
+    client.on('disconnect', function (packet) {
+      client.stream.end();
+    });
+  });
+  server.listen(PORT);
+  return server;
+};
+
