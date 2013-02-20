9d5683d063f203fd988a9912a108d555065dbdee
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-02-20 18:40:22 +0100

    Test refactor and tests for connacks

--- test/client.js
+++ test/client.js
@@ -23,51 +23,79 @@ describe('MqttClient', function () {
     this.server = mqtt.createServer();
     this.server.listen(port);
   });
-  describe('#constructor', function () {
+  it('should connect to the broker', function (done) {
+    var client = new MqttClient(port);
 
-    it('should connect to the broker', function (done) {
-      var client = new MqttClient(port);
+    this.server.once('client', function(client) {
+      done();
+    });
 
-      this.server.once('client', function(client) {
+  }); 
+
+  it('should send a default client id', function (done) {
+    var client = new MqttClient(port);
+
+    this.server.once('client', function (client) {
+      client.once('connect', function(packet) {
+        packet.client.should.match(/mqttjs.*/);
         done();
       });
-
-    }); 
-
-    it('should send a default client id', function (done) {
-      var client = new MqttClient(port);
-
-      this.server.once('client', function (client) {
-        client.once('connect', function(packet) {
-          packet.client.should.match(/mqttjs.*/);
-          done();
-        });
-      });
-
     });
 
-    it('should connect with the given client id', function (done) {
-      var client = 
-        new MqttClient(port, 'localhost', {client: 'testclient'});
+  });
 
-      this.server.once('client', function (client) {
-        client.once('connect', function(packet) {
-          packet.client.should.match(/testclient/);
-          done();
-        });
-      });
-    });
-    it('should default to localhost', function (done) {
-      var client = new MqttClient(port, {client: 'testclient'});
+  it('should connect with the given client id', function (done) {
+    var client = 
+      new MqttClient(port, 'localhost', {client: 'testclient'});
 
-      this.server.once('client', function (client) {
-        client.once('connect', function(packet) {
-          packet.client.should.match(/testclient/);
-          done();
-        });
+    this.server.once('client', function (client) {
+      client.once('connect', function(packet) {
+        packet.client.should.match(/testclient/);
+        done();
       });
     });
   });
+
+  it('should default to localhost', function (done) {
+    var client = new MqttClient(port, {client: 'testclient'});
+
+    this.server.once('client', function (client) {
+      client.once('connect', function(packet) {
+        packet.client.should.match(/testclient/);
+        done();
+      });
+    });
+  });
+
+  it('should emit connect', function (done) {
+    var client = new MqttClient(port);
+    client.once('connect', done);
+    client.once('error', done);
+
+    this.server.once('client', function(client) {
+      client.once('connect', function(packet) {
+        client.connack({returnCode: 0});
+      });
+    });
+  });
+
+  it('should emit error', function (done) {
+    var client = new MqttClient(port);
+    client.once('connect', function () {
+      done(new Error('Should not emit connect'));
+    });
+    client.once('error', function(error) {
+      done();
+    });
+
+    this.server.once('client', function(client) {
+      client.once('connect', function(packet) {
+        client.connack({returnCode: 2});
+      });
+    });
+
+  });
+
   after(function () {
     this.server.close();
   });
