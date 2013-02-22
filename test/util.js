fd457559bf22182bcb49eb5439dadc06a1d9928b
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-02-22 11:07:28 +0100

    Added #setEncoding to TestStream

--- test/util.js
+++ test/util.js
@@ -13,10 +13,19 @@ var TestStream = module.exports.TestStream =
 function TestStream() {
   if (!this instanceof TestStream) return new TestStream();
 
+  this.encoding = null;
+
   Stream.apply(this);
 };
 util.inherits(TestStream, Stream);
 
 TestStream.prototype.write = function(buffer) {
+  if (this.encoding) {
+    buffer = buffer.toString(this.encoding);
+  }
   process.nextTick(this.emit.bind(this, 'data', buffer));
+};
+
+TestStream.prototype.setEncoding = function(encoding) {
+  this.encoding = encoding;
 };
