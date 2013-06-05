e553b64321b04e1aa8b47125f2adf22520c1c332
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-06-05 15:54:58 +0200

    Test using a fake correct functioning server
    One that acts mostly to mqtt spec

--- test/client.js
+++ test/client.js
@@ -13,12 +13,65 @@ var createClient = require('../lib/mqtt').createClient;
 /**
  * Testing options
  */
-
 var port = 9876;
 
 describe('MqttClient', function () {
   before(function () {
     this.server = mqtt.createServer();
+
+    this.server.on('client', function(client) {
+      var server = this;
+
+      client.on('connect', function(packet) {
+        if (packet.clientId === 'invalid') {
+          client.connack({returnCode: 2});
+        } else {
+          client.connack({returnCode: 0});
+        }
+      });
+
+      client.on('publish', function (packet) {
+        switch (packet.qos) {
+          case 0:
+            break;
+          case 1:
+            client.puback(packet);
+            break;
+          case 2:
+            client.pubrec(packet);
+            break;
+        }
+      });
+
+      client.on('pubrel', function(packet) {
+        client.pubcomp(packet);
+      });
+
+      client.on('pubrec', function(packet) {
+        client.pubrel(packet);
+      });
+
+      client.on('pubcomp', function(packet) {
+        // Nothing to be done
+      });
+
+      client.on('subscribe', function(packet) {
+        client.suback({
+          messageId: packet.messageId,
+          granted: packet.subscriptions.map(function (e) {
+            return e.qos;
+          })
+        });
+      });
+
+      client.on('unsubscribe', function(packet) {
+        client.unsuback(packet);
+      });
+
+      client.on('pingreq', function(packet) {
+        client.pingresp();
+      });
+    });
     this.server.listen(port);
   });
 
