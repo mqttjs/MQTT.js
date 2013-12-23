591737b637cb745fc74dcc10fbfd24131e864cd5
Elliott Ye <elliott.ye@mesheven.com>
2013-12-23 11:57:18 +0800

    Fixed the unit test that tests the reconnect logic of a client.

--- test/client.fault_tolerance.js
+++ test/client.fault_tolerance.js
@@ -1,34 +1,26 @@
 var should = require('should'),
-    mqtt   = require('../lib/mqtt');
+  mqtt   = require('../lib/mqtt');
 
 describe('Fault tolerance', function () {
 
-  describe('reconnect', function () {
+  describe('reconnecting', function () {
     it('should attempt to reconnect once server is down', function (done) {
       this.timeout(15000);
 
       var fork   = require('child_process').fork;
       var server = fork(__dirname + '/helpers/server_process.js');
 
-      var client = mqtt.createClient('3000', 'localhost', {
-        keepalive: 2 // times 600 ms
-      });
+      var client = mqtt.createClient('3000', 'localhost', { keepalive: 1 });
 
-      client.on('connect', function () {
-        should.not.exist(client.reconnectTimer);
-        should(server.kill('SIGINT')); // mocks server shutdown
+      client.once('connect', function () {
+        server.kill('SIGINT'); // mocks server shutdown
 
-        setTimeout(function () {
-          should.exist(client.reconnectTimer); // client attempts to reconnect
+        client.once('close', function () {
+          should.exist(client.reconnectTimer);
           done();
-        }, 2000);
+        });
       });
     });
-
-//    it('should attempt to reconnect once network cable is unplugged', function (done) {
-//      // TODO
-//      done();
-//    });
   });
 
 });
