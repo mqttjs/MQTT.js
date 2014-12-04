08c9723cbdc5de04c614c4c1a1bdcc1e2059890b
Matteo Collina <hello@matteocollina.com>
2014-12-04 10:00:02 +0100

    Added an async store for inflight packets.

---
+++ test/store.js
@@ -0,0 +1,91 @@
+
+var should = require('should')
+  , mqtt = require("../")
+  , Store = require("../lib/store");
+
+describe("inflight store", function() {
+  var store;
+
+  beforeEach(function() {
+    store = new Store();
+  });
+
+  it("should put and stream in-flight packets", function(done) {
+    var packet = {
+      topic: "hello",
+      payload: "world",
+      qos: 1,
+      messageId: 42
+    };
+
+    store.put(packet, function() {
+      store
+        .createStream()
+        .on('data', function(data) {
+          data.should.equal(packet)
+          done()
+        })
+    })
+  });
+
+  it("should add and del in-flight packets", function(done) {
+    var packet = {
+      topic: "hello",
+      payload: "world",
+      qos: 1,
+      messageId: 42
+    };
+
+    store.put(packet, function() {
+      store.del(packet, function() {
+        store
+          .createStream()
+          .on("data", function(data) {
+            done(new Error("this should never happen"));
+          })
+          .on("end", done);
+      });
+    });
+  });
+
+  it("should replace a packet when doing put with the same messageId", function(done) {
+    var packet1 = {
+      topic: "hello",
+      payload: "world",
+      qos: 2,
+      messageId: 42
+    };
+
+    var packet2 = {
+      qos: 2,
+      messageId: 42
+    };
+
+    store.put(packet1, function() {
+      store.put(packet2, function() {
+        store
+          .createStream()
+          .on('data', function(data) {
+            data.should.equal(packet2)
+            done()
+          });
+      });
+    });
+  });
+
+  it("should return the original packet on del", function(done) {
+    var packet = {
+      topic: "hello",
+      payload: "world",
+      qos: 1,
+      messageId: 42
+    };
+
+    store.put(packet, function() {
+      store.del({ messageId: 42 }, function(err, deleted) {
+        deleted.should.equal(packet);
+        done();
+      });
+    });
+  });
+});
