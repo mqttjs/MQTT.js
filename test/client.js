333c75e852a185d2bb9c794857f797228e1e85ca
RangerMauve <RangerMauve@hotmail.com>
2015-06-10 17:27:33 -0400

    Test for connack working with reconnect

--- test/client.js
+++ test/client.js
@@ -199,5 +199,36 @@ describe('MqttClient', function () {
         });
       });
     });
+
+    it('shoud not be cleared by the connack timer', function (done) {
+      this.timeout(4000);
+
+      var server2 = net.createServer().listen(port + 44);
+
+      server2.on('connection', function (c) {
+        c.destroy();
+      });
+
+      server2.once('listening', function () {
+        var reconnects = 0;
+        var connectTimeout = 1000;
+        var reconnectPeriod = 100;
+        var expectedReconnects = Math.floor(connectTimeout / reconnectPeriod);
+
+        var client = mqtt.connect({
+          port: port + 44,
+          host: 'localhost',
+          connectTimeout: connectTimeout,
+          reconnectPeriod: reconnectPeriod });
+
+        client.on('reconnect', function () {
+          reconnects++;
+          if (reconnects >= expectedReconnects) {
+            client.end();
+            done();
+          }
+        });
+      });
+    });
   });
 });
