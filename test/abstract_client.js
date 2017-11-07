512c0398d3b79d13db27eedc41813b3f3edb19e7
prabathabey <prabathabeysekara@gmail.com>
2017-11-07 14:02:31 +1100

    Rewrote the test-case that handles transactionality of QoS 2 messages when the execution of 'handleMessage' fails to avoid potentially flaky results

--- test/abstract_client.js
+++ test/abstract_client.js
@@ -8,6 +8,7 @@ var sinon = require('sinon')
 var mqtt = require('../')
 var xtend = require('xtend')
 var Server = require('./server')
+var Store = require('./../lib/store')
 var port = 9876
 
 module.exports = function (server, config) {
@@ -794,7 +795,8 @@ module.exports = function (server, config) {
         messageId: Math.floor(65535 * Math.random()),
         topic: 'test',
         payload: 'test',
-        qos: 1}, function (err) {
+        qos: 1
+      }, function (err) {
         should.exist(err)
       })
 
@@ -803,8 +805,9 @@ module.exports = function (server, config) {
       done()
     })
 
-    it('should not send a `pubrel` if the execution of `handleMessage` fails for messages with QoS `2`', function (done) {
-      var client = connect()
+    it('should not send a `pubcomp` if the execution of `handleMessage` fails for messages with QoS `2`', function (done) {
+      var store = new Store()
+      var client = connect({incomingStore: store})
 
       var messageId = Math.floor(65535 * Math.random())
       var topic = 'test'
@@ -815,35 +818,25 @@ module.exports = function (server, config) {
         callback(new Error('Error thrown by the application'))
       }
 
-      client.on('connect', function () {
-        client.subscribe(topic)
+      client.once('connect', function () {
+        client.subscribe(topic, {qos: 2})
 
-        client.on('packetreceive', function (packet) {
-          if (packet.cmd && packet.cmd === 'pubrel') {
-            client._sendPacket = sinon.spy()
+        store.put({
+          messageId: messageId,
+          topic: topic,
+          payload: payload,
+          qos: qos,
+          cmd: 'publish'
+        }, function () {
+          // cleans up the client
+          client.end()
 
-            client._handlePubrel(packet, function (err) {
-              should.exist(err)
-            })
-
-            client._sendPacket.callCount.should.equal(0)
-            client.end()
-            done()
-          }
-        })
-      })
-
-      server.once('client', function (serverClient) {
-        serverClient.on('subscribe', function () {
-          serverClient.publish({
-            messageId: messageId,
-            topic: topic,
-            payload: payload,
-            qos: qos
+          client._sendPacket = sinon.spy()
+          client._handlePubrel({cmd: 'pubrel', messageId: messageId}, function (err) {
+            should.exist(err)
           })
-        })
-        serverClient.on('puback', function () {
-          done('Unexpected `puback` received.')
+          client._sendPacket.callCount.should.equal(0)
+          done()
         })
       })
     })
