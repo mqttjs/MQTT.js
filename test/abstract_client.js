43d3258414bda8bc893beb2788342bdf83c95656
Takatoshi Kondo <redboltz@gmail.com>
2017-12-06 14:03:05 +0900

    Updated deferring condition.
    Defer reconnecting only the cae if during disconnecting but not disconnected yet.
    Added test for NOT deferring reconnect().

--- test/abstract_client.js
+++ test/abstract_client.js
@@ -2058,6 +2058,38 @@ module.exports = function (server, config) {
       })
     })
 
+    it('should be able to pub/sub if reconnect() is called at out of close handler', function (done) {
+      var client = connect({ reconnectPeriod: 0 })
+      var tryReconnect = true
+      var reconnectEvent = false
+
+      client.on('close', function () {
+        if (tryReconnect) {
+          tryReconnect = false
+          setTimeout(function () {
+            client.reconnect()
+          }, 100)
+        } else {
+          reconnectEvent.should.equal(true)
+          done()
+        }
+      })
+
+      client.on('reconnect', function () {
+        reconnectEvent = true
+      })
+
+      client.on('connect', function () {
+        if (tryReconnect) {
+          client.end()
+        } else {
+          client.subscribe('hello', function () {
+            client.end()
+          })
+        }
+      })
+    })
+
     context('with alternate server client', function () {
       var cachedClientListeners
 
