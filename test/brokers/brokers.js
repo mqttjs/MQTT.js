13da66842cab0231c6f4539ed67d6f2600ad5a53
4rzael <maxime.agor23@gmail.com>
2018-01-26 12:08:05 +0000

    Added travis env for integration tests

--- test/brokers/brokers.js
+++ test/brokers/brokers.js
@@ -3,190 +3,191 @@
 const mqtt = require('../../')
 const brokers = require('./brokers.json')
 const should = require('should')
+should() // to disable unused jslint error
 
-function buildUrl(protocol) {
+function buildUrl (protocol) {
   return protocol.name + '://localhost:' + protocol.port + (protocol.route || '')
 }
 
-describe('broker compatibility', () => {
-  brokers.forEach(broker => {
-    describe(broker.name, () => {
-      broker.protocols.forEach(protocol => {
-        describe('protocol ' + protocol.name, () => {
+if (process.env.RUN_BROKER_TESTS) {
+  describe('broker compatibility', () => {
+    brokers.forEach(broker => {
+      describe(broker.name, () => {
+        broker.protocols.forEach(protocol => {
+          describe('protocol ' + protocol.name, () => {
+            let client
+            const pretopic = '/' + broker.name + '/' + protocol.name
 
-          let client
-          const pretopic = '/' + broker.name + '/' + protocol.name
+            afterEach(() => {
+              if (client) {
+                client.end()
+                client = undefined
+              }
+            })
 
-          afterEach(() => {
-            if (client) {
-              client.end(true)
-              client = undefined
+            it('should be able to connect to the broker', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              client.on('connect', () => {
+                client.connected.should.equal(true)
+                done()
+              })
+            })
+
+            it('should be able to disconnect from the broker', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              client.on('connect', () => {
+                client.end(true)
+              })
+              client.on('close', () => {
+                done()
+              })
+            })
+
+            it('should be able to publish in QoS 0', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              client.on('connect', () => {
+                client.subscribe(pretopic + '/testqos0', () => {
+                  client.publish(pretopic + '/testqos0', 'QoS 0 works')
+                })
+              })
+              client.on('message', (topic, content, packet) => {
+                topic.should.equal(pretopic + '/testqos0')
+                content.toString().should.equal('QoS 0 works')
+                packet.qos.should.equal(0)
+                done()
+              })
+            })
+
+            it('should be able to publish in QoS 1', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              const receivedPubAck = new Promise((resolve, reject) => {
+                client.on('connect', () => {
+                  client.subscribe(pretopic + '/testqos1', {qos: 1}, () => {
+                    client.publish(pretopic + '/testqos1', 'QoS 1 works', {qos: 1},
+                      () => resolve())
+                  })
+                })
+              })
+              const receivedMessage = new Promise((resolve, reject) => {
+                client.on('message', (topic, content, packet) => {
+                  topic.should.equal(pretopic + '/testqos1')
+                  content.toString().should.equal('QoS 1 works')
+                  packet.qos.should.equal(1)
+                  resolve()
+                })
+              })
+
+              Promise.all([receivedPubAck, receivedMessage])
+              .then(() => {
+                done()
+              })
+            })
+
+            if (!broker.NO_QOS2) {
+              it('should be able to publish in QoS 2', (done) => {
+                client = mqtt.connect(buildUrl(protocol))
+                const receivedPubAck = new Promise((resolve, reject) => {
+                  client.on('connect', () => {
+                    client.subscribe(pretopic + '/testqos2', {qos: 2}, () => {
+                      client.publish(pretopic + '/testqos2', 'QoS 2 works', {qos: 2},
+                        () => resolve())
+                    })
+                  })
+                })
+
+                const receivedMessage = new Promise((resolve, reject) => {
+                  client.on('message', (topic, content, packet) => {
+                    topic.should.equal(pretopic + '/testqos2')
+                    content.toString().should.equal('QoS 2 works')
+                    packet.qos.should.equal(2)
+                    resolve()
+                  })
+                })
+
+                Promise.all([receivedPubAck, receivedMessage])
+                .then(() => {
+                  done()
+                })
+              })
             }
-          });
 
-          it('should be able to connect to the broker', (done) => {
-            client = mqtt.connect(buildUrl(protocol))
-            client.on('connect', () => {
-              client.connected.should.be.true
-              done()
+            it('should be able to downgrade the QoS while subscribing', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              client.on('connect', () => {
+                client.subscribe(pretopic + '/testqosdowngrade', {qos: 0})
+                client.publish(pretopic + '/testqosdowngrade', 'QoS downgrade works', {qos: 2})
+              })
+              client.on('message', (topic, content, packet) => {
+                topic.should.equal(pretopic + '/testqosdowngrade')
+                content.toString().should.equal('QoS downgrade works')
+                packet.qos.should.equal(0)
+                done()
+              })
+            })
+
+            it('should be able to unsubscribe', (done) => {
+              client = mqtt.connect(buildUrl(protocol))
+              let messagesReceived = 0
+
+              client.on('connect', () => {
+                client.subscribe(pretopic + '/testunsubscribe', () => {
+                  client.publish(pretopic + '/testunsubscribe', 'First Message')
+                })
+              })
+
+              client.on('message', (topic, content, packet) => {
+                messagesReceived.should.equal(0)
+                ++messagesReceived
+                client.unsubscribe(pretopic + '/testunsubscribe', () => {
+                  client.publish('testunsubscribe', 'Second Message', () => {
+                    setTimeout(() => {
+                      done()
+                    }, 200) // Wait for the message to arrive
+                  })
+                })
+              })
+            })
+
+            it('should handle last-will', (done) => {
+              const dyingClient = mqtt.connect(buildUrl(protocol), {
+                will: {
+                  topic: pretopic + '/lwt',
+                  payload: 'byebye',
+                  qos: 1
+                },
+                keepalive: 1
+              })
+
+              client = mqtt.connect(buildUrl(protocol))
+
+              const dyingConnected = new Promise((resolve, reject) => {
+                dyingClient.on('connect', () => {
+                  resolve()
+                })
+              })
+              const clientConnected = new Promise((resolve, reject) => {
+                client.on('connect', () => {
+                  resolve()
+                })
+              })
+
+              Promise.all([dyingConnected, clientConnected])
+              .then(() => {
+                client.on('message', (topic, content) => {
+                  console.log('message received')
+                  topic.should.equal(pretopic + '/lwt')
+                  content.toString().should.equal('byebye')
+                  done()
+                })
+
+                client.subscribe(pretopic + '/lwt', () => {
+                  dyingClient.end(true) // simulates a disconnection
+                })
+              })
             })
           })
-
-          // it('should be able to disconnect from the broker', (done) => {
-          //   client = mqtt.connect(buildUrl(protocol))
-          //   client.on('connect', () => {
-          //     client.end(true)
-          //   })
-          //   client.on('close', () => {
-          //     done()
-          //   })
-          // })
-          //
-          // it('should be able to publish in QoS 0', (done) => {
-          //   client = mqtt.connect(buildUrl(protocol))
-          //   client.on('connect', () => {
-          //     client.subscribe(pretopic + '/testqos0', () => {
-          //       client.publish(pretopic + '/testqos0', 'QoS 0 works')
-          //     })
-          //   })
-          //   client.on('message', (topic, content, packet) => {
-          //     topic.should.equal(pretopic + '/testqos0')
-          //     content.toString().should.equal('QoS 0 works')
-          //     packet.qos.should.equal(0)
-          //     done()
-          //   })
-          // })
-          //
-          // it('should be able to publish in QoS 1', (done) => {
-          //   client = mqtt.connect(buildUrl(protocol))
-          //   const receivedPubAck = new Promise((accept, reject) => {
-          //     client.on('connect', () => {
-          //       client.subscribe(pretopic + '/testqos1', {qos: 1}, () => {
-          //         client.publish(pretopic + '/testqos1', 'QoS 1 works', {qos: 1},
-          //           () => accept())
-          //       })
-          //     })
-          //   })
-          //   const receivedMessage = new Promise((accept, reject) => {
-          //     client.on('message', (topic, content, packet) => {
-          //       topic.should.equal(pretopic + '/testqos1')
-          //       content.toString().should.equal('QoS 1 works')
-          //       packet.qos.should.equal(1)
-          //       accept()
-          //     })
-          //   })
-          //
-          //   Promise.all([receivedPubAck, receivedMessage])
-          //   .then(() => {
-          //     done()
-          //   })
-          // })
-          //
-          // if (!broker.NO_QOS2) {
-          //   it('should be able to publish in QoS 2', (done) => {
-          //     client = mqtt.connect(buildUrl(protocol))
-          //     const receivedPubAck = new Promise((accept, reject) => {
-          //       client.on('connect', () => {
-          //         client.subscribe(pretopic + '/testqos2', {qos: 2}, () => {
-          //           client.publish(pretopic + '/testqos2', 'QoS 2 works', {qos: 2},
-          //             () => accept())
-          //         })
-          //       })
-          //     })
-          //
-          //     const receivedMessage = new Promise((accept, reject) => {
-          //       client.on('message', (topic, content, packet) => {
-          //         topic.should.equal(pretopic + '/testqos2')
-          //         content.toString().should.equal('QoS 2 works')
-          //         packet.qos.should.equal(2)
-          //         accept()
-          //       })
-          //     })
-          //
-          //     Promise.all([receivedPubAck, receivedMessage])
-          //     .then(() => {
-          //       done()
-          //     })
-          //   })
-          // }
-          //
-          // it('should be able to downgrade the QoS while subscribing', (done) => {
-          //   client = mqtt.connect(buildUrl(protocol))
-          //   client.on('connect', () => {
-          //     client.subscribe(pretopic + '/testqosdowngrade', {qos: 0})
-          //     client.publish(pretopic + '/testqosdowngrade', 'QoS downgrade works', {qos: 2})
-          //   })
-          //   client.on('message', (topic, content, packet) => {
-          //     topic.should.equal(pretopic + '/testqosdowngrade')
-          //     content.toString().should.equal('QoS downgrade works')
-          //     packet.qos.should.equal(0)
-          //     done()
-          //   })
-          // })
-          //
-          // it('should be able to unsubscribe', (done) => {
-          //   client = mqtt.connect(buildUrl(protocol))
-          //   let messagesReceived = 0
-          //
-          //   client.on('connect', () => {
-          //     client.subscribe(pretopic + '/testunsubscribe', () => {
-          //       client.publish(pretopic + '/testunsubscribe', 'First Message')
-          //     })
-          //   })
-          //
-          //   client.on('message', (topic, content, packet) => {
-          //     messagesReceived.should.equal(0)
-          //     ++messagesReceived
-          //     client.unsubscribe(pretopic + '/testunsubscribe', () => {
-          //       client.publish('testunsubscribe', 'Second Message', () => {
-          //         setTimeout(() => {
-          //           done()
-          //         }, 200) // Wait for the message to arrive
-          //       })
-          //     })
-          //   })
-          // })
-          //
-          // it('should handle last-will', (done) => {
-          //   const dyingClient = mqtt.connect(buildUrl(protocol),
-          //   {
-          //     will: {
-          //       topic: pretopic + '/lwt',
-          //       payload: 'byebye',
-          //       qos: 2
-          //     },
-          //     keepalive: 1
-          //   })
-          //
-          //   client = mqtt.connect(buildUrl(protocol))
-          //
-          //   const dyingConnected = new Promise((accept, reject) => {
-          //     dyingClient.on('connect', () => {
-          //       accept()
-          //     })
-          //   })
-          //   const clientConnected = new Promise((accept, reject) => {
-          //     client.on('connect', () => {
-          //       accept()
-          //     })
-          //   })
-          //
-          //   Promise.all([dyingConnected, clientConnected])
-          //   .then(() => {
-          //     client.on('message', (topic, content) => {
-          //       console.log('message received')
-          //       topic.should.equal(pretopic + '/lwt')
-          //       content.toString().should.equal('byebye')
-          //       done()
-          //     })
-          //
-          //     client.subscribe(pretopic + '/lwt', () => {
-          //       dyingClient.end(true)
-          //     })
-          //   })
-          // })
         })
       })
     })
   })
-})
+}
--- test/brokers/brokers.sh
+++ test/brokers/brokers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # mosca
-docker run --rm -d --name mosca -p 2083:1883 -p 2080:80 -v /var/db/mosca:/db matteocollina/mosca
+docker run --rm -d --name mosca -p 2001:1883 -p 2002:80 -v /var/db/mosca:/db matteocollina/mosca
 # mosquitto
-docker run --rm -d --name mosquitto eclipse-mosquitto -p 2183:1883 2180:80
+docker run --rm -d --name mosquitto -p 2011:1883 -p 2012:9001 toke/mosquitto
 # emq
-docker run --rm -d --name emq -p 2022:8083 -p 2021:1883 sneck/emqttd:latest
+docker run --rm -d --name emq -p 2021:1883 -p 2022:8083 sneck/emqttd:latest
