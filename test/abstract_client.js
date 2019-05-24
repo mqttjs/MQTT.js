2cb1ca44815bdc42c250e2e24568897ebd839229
Takatoshi Kondo <redboltz@gmail.com>
2019-05-24 21:50:52 +0900

    Clear `outgoing` on close.
    If `outgoing` was is cleared on close from the server, there is no
    chance to call callbacks in `outgoing`. `outgoing` entries exist
    invalidly after closing.
    
    That prevents `end` function. In the `end` function, if `force` is not
    set and `outgoing` exists, then wait `outgoingEmpty` event. However,
    the event never happen because previous entries are not removed.
    
    This update fixes the problem.

--- test/abstract_client.js
+++ test/abstract_client.js
@@ -2682,6 +2682,55 @@ module.exports = function (server, config) {
       })
     })
 
+    it.only('should clear outgoing if close from server', function (done) {
+      var reconnect = false
+      var client = {}
+      var server2 = new Server(function (c) {
+        c.on('connect', function (packet) {
+          c.connack({returnCode: 0})
+        })
+        c.on('subscribe', function (packet) {
+          if (reconnect) {
+            c.suback({
+              messageId: packet.messageId,
+              granted: packet.subscriptions.map(function (e) {
+                return e.qos
+              })
+            })
+          } else {
+            c.destroy()
+          }
+        })
+      })
+
+      server2.listen(port + 50, function () {
+        client = mqtt.connect({
+          port: port + 50,
+          host: 'localhost',
+          clean: true,
+          clientId: 'cid1',
+          reconnectPeriod: 0
+        })
+
+        client.on('connect', function () {
+          client.subscribe('test', {qos: 2}, function () {
+            client.end()
+          })
+        })
+
+        client.on('close', function () {
+          if (reconnect) {
+            server2.close()
+            done()
+          } else {
+            Object.keys(client.outgoing).length.should.equal(0)
+            reconnect = true
+            client.reconnect()
+          }
+        })
+      })
+    })
+
     it('should resend in-flight QoS 1 publish messages from the client if clean is false', function (done) {
       var reconnect = false
       var client = {}
