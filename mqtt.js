e3ea11a2f941ea1a5c98f3944311e41ac7e30da2
Alexis Tyler <xo@wvvw.me>
2020-05-11 15:09:18 +0930

    chore: move cli to bin directory (#1096)

--- mqtt.js
+++ mqtt.js
@@ -1,6 +1,3 @@
-#!/usr/bin/env node
-'use strict'
-
 /*
  * Copyright (c) 2015-2015 MQTT.js contributors.
  * Copyright (c) 2011-2014 Adam Rudd.
@@ -18,24 +15,3 @@ module.exports.connect = connect
 module.exports.MqttClient = MqttClient
 module.exports.Client = MqttClient
 module.exports.Store = Store
-
-function cli () {
-  var commist = require('commist')()
-  var helpMe = require('help-me')()
-
-  commist.register('publish', require('./bin/pub'))
-  commist.register('subscribe', require('./bin/sub'))
-  commist.register('version', function () {
-    console.log('MQTT.js version:', require('./package.json').version)
-  })
-  commist.register('help', helpMe.toStdout)
-
-  if (commist.parse(process.argv.slice(2)) !== null) {
-    console.log('No such command:', process.argv[2], '\n')
-    helpMe.toStdout()
-  }
-}
-
-if (require.main === module) {
-  cli()
-}
