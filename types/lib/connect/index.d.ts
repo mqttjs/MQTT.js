433bb0ec56e5d977f37ef3aa3bb2074682e1b0bb
Edward Richardson-Lee <77331675+erichardson-lee@users.noreply.github.com>
2022-02-08 09:54:15 +0000

    Fix the typings for the connect function to have the proper overloads for its parameters

--- types/lib/connect/index.d.ts
+++ types/lib/connect/index.d.ts
@@ -1,10 +1,25 @@
-import { IClientOptions, MqttClient } from '../client'
+import { IClientOptions, MqttClient } from "../client";
 /**
  * connect - connect to an MQTT broker.
  *
- * @param {String} [brokerUrl] - url of the broker, optional
+ * @param {String} brokerUrl - url of the broker
+ */
+declare function connect(brokerUrl: string): MqttClient;
+
+/**
+ * connect - connect to an MQTT broker.
+ *
  * @param {Object} opts - see MqttClient#constructor
  */
-declare function connect (brokerUrl?: string | any, opts?: IClientOptions): MqttClient
-export { connect }
-export { MqttClient }
+declare function connect(opts: IClientOptions): MqttClient;
+
+/**
+ * connect - connect to an MQTT broker.
+ *
+ * @param {String} brokerUrl - url of the broker
+ * @param {Object} opts - see MqttClient#constructor
+ */
+declare function connect(brokerUrl: string, opts: IClientOptions): MqttClient;
+
+export { connect };
+export { MqttClient };
