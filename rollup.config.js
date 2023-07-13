e200ee218e320ceab7c8ad2450d1e5005adea4f1
Daniel Lando <daniel.sorridi@gmail.com>
2023-07-13 10:06:56 +0200

    chore: readable-stream and mqtt-packet patches

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,46 @@
 const commonjs = require('@rollup/plugin-commonjs');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const polyfills = require('rollup-plugin-node-polyfills');
+const replace = require('@rollup/plugin-replace');
+const fs = require('fs');
+
+// patch mqtt packet parser
+const mqttPacketParsePath = './node_modules/mqtt-packet/parser.js';
+const mqttPacketParseContent = fs.readFileSync(mqttPacketParsePath, 'utf8');
+const modifiedMqttPacketParseContent = mqttPacketParseContent.replace('const EventEmitter = require(\'events\')', 'const {EventEmitter} = require(\'events\')');
+fs.writeFileSync(mqttPacketParsePath, modifiedMqttPacketParseContent, 'utf8');
 
 module.exports = {
     input: './lib/connect/index.js',
+    // input: './mqtt.js',
     output: {
         format: 'umd',
         file: 'dist/mqtt.js',
         name: 'mqtt',
         globals: {
-            //lodash: '_',
+            'readable-stream': 'stream',
         },
         exports: 'named',
     },
     plugins: [
-        commonjs(),
         nodeResolve({
             preferBuiltins: true,
             browser: true,
             mainFields: ['module', 'main', 'browser'],
         }),
+        replace({
+            preventAssignment: true,
+            values: {
+                'require(\'readable-stream\')': 'require(\'stream\')',
+            }
+        }),
+        commonjs({
+            transformMixedEsModules: true
+        }),
         polyfills(),
+    ],
+    external: [
+        'readable-stream',
     ],
     context: 'window'
 };
