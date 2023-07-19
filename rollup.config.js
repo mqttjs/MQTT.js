e1144396cdde4c65e46e7864904b8217c774dad7
Daniel Lando <daniel.sorridi@gmail.com>
2023-07-19 15:23:07 +0200

    chore: use newer node polifilly

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,48 @@
-const commonjs = require('@rollup/plugin-commonjs');
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const polyfills = require('rollup-plugin-node-polyfills');
-const replace = require('@rollup/plugin-replace');
-const fs = require('fs');
+/* eslint-disable import/no-extraneous-dependencies */
+const commonjs = require('@rollup/plugin-commonjs')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const polyfills = require('rollup-plugin-polyfill-node')
+const replace = require('@rollup/plugin-replace')
+const fs = require('fs')
 
 // patch mqtt packet parser
-const mqttPacketParsePath = './node_modules/mqtt-packet/parser.js';
-const mqttPacketParseContent = fs.readFileSync(mqttPacketParsePath, 'utf8');
-const modifiedMqttPacketParseContent = mqttPacketParseContent.replace('const EventEmitter = require(\'events\')', 'const {EventEmitter} = require(\'events\')');
-fs.writeFileSync(mqttPacketParsePath, modifiedMqttPacketParseContent, 'utf8');
+const mqttPacketParsePath = './node_modules/mqtt-packet/parser.js'
+const mqttPacketParseContent = fs.readFileSync(mqttPacketParsePath, 'utf8')
+const modifiedMqttPacketParseContent = mqttPacketParseContent.replace(
+	"const EventEmitter = require('events')",
+	"const {EventEmitter} = require('events')",
+)
+fs.writeFileSync(mqttPacketParsePath, modifiedMqttPacketParseContent, 'utf8')
 
 module.exports = {
-    input: './lib/connect/index.js',
-    // input: './mqtt.js',
-    output: {
-        format: 'umd',
-        file: 'dist/mqtt.js',
-        name: 'mqtt',
-        globals: {
-            'readable-stream': 'stream',
-        },
-        exports: 'named',
-    },
-    plugins: [
-        nodeResolve({
-            preferBuiltins: true,
-            browser: true,
-            mainFields: ['module', 'main', 'browser'],
-        }),
-        replace({
-            preventAssignment: true,
-            values: {
-                'require(\'readable-stream\')': 'require(\'stream\')',
-            }
-        }),
-        commonjs({
-            transformMixedEsModules: true
-        }),
-        polyfills(),
-    ],
-    external: [
-        'readable-stream',
-    ],
-    context: 'window'
-};
+	input: './lib/connect/index.js',
+	// input: './mqtt.js',
+	output: {
+		format: 'umd',
+		file: 'dist/mqtt.js',
+		name: 'mqtt',
+		globals: {
+			'readable-stream': 'stream',
+		},
+		exports: 'named',
+	},
+	plugins: [
+		nodeResolve({
+			preferBuiltins: true,
+			browser: true,
+			mainFields: ['module', 'main', 'browser'],
+		}),
+		replace({
+			preventAssignment: true,
+			values: {
+				"require('readable-stream')": "require('stream')",
+			},
+		}),
+		commonjs({
+			transformMixedEsModules: true,
+		}),
+		polyfills(),
+	],
+	external: ['readable-stream'],
+	context: 'window',
+}
