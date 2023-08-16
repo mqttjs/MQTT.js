183b35aa3ed98fbbcbea6805994ef7c3cc8ee616
Daniel Lando <daniel.sorridi@gmail.com>
2023-08-16 14:13:40 +0200

    fix: browser detection
    Fixes #1671

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,12 +1,4 @@
-const legacyIsBrowser =
-	(typeof process !== 'undefined' && process.title === 'browser') ||
-	// eslint-disable-next-line camelcase, @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	typeof __webpack_require__ === 'function'
-
 const isBrowser =
-	typeof window !== 'undefined' && typeof document !== 'undefined'
+	typeof window !== 'undefined' && typeof window.document !== 'undefined'
 
-const IS_BROWSER = isBrowser || legacyIsBrowser
-
-export default IS_BROWSER
+export default isBrowser
