588c914893bfd22f56ec67a30e246facbd741349
Daniel Lando <daniel.sorridi@gmail.com>
2023-09-19 17:15:31 +0200

    fix: switch to spec

--- test/runTests.ts
+++ test/runTests.ts
@@ -1,13 +1,16 @@
 import { readdirSync } from 'node:fs'
 import { run } from 'node:test'
 import process from 'node:process'
-import { tap } from 'node:test/reporters'
+import { spec as Spec } from 'node:test/reporters'
 import { basename } from 'node:path'
+import { cpus } from 'node:os'
+
+const spec = new Spec()
 
 const files = readdirSync(__dirname)
 	.filter((f) => f.endsWith('.ts') && f !== basename(__filename))
 	.map((f) => `${__dirname}/${f}`)
 
-run({ files, timeout: 60 * 1000, concurrency: true })
-	.compose(tap)
+run({ files, timeout: 60 * 1000, concurrency: cpus().length })
+	.compose(spec)
 	.pipe(process.stdout)
