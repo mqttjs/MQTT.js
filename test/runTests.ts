bd471779213636a4383dd58fa9e86843360bf606
Daniel Lando <daniel.sorridi@gmail.com>
2023-09-19 17:57:41 +0200

    fix: exit code on error

--- test/runTests.ts
+++ test/runTests.ts
@@ -7,10 +7,37 @@ import { cpus } from 'node:os'
 
 const spec = new Spec()
 
+let exitCode = 0
+
 const files = readdirSync(__dirname)
 	.filter((f) => f.endsWith('.ts') && f !== basename(__filename))
 	.map((f) => `${__dirname}/${f}`)
 
-run({ files, timeout: 60 * 1000, concurrency: cpus().length })
-	.compose(spec)
-	.pipe(process.stdout)
+const testStream = run({
+	files,
+	timeout: 60 * 1000,
+	concurrency: cpus().length,
+})
+
+testStream.compose(spec).pipe(process.stdout)
+
+const summary: string[] = []
+
+testStream.once('test:fail', (data) => {
+	exitCode = 1
+	summary.push(`${data.file} - Test ${data.name} failed:\n${data.details} `)
+})
+
+testStream.once('test:stderr', (data) => {
+	exitCode = 1
+	summary.push(`${data.file} - Error:\n${data.message} `)
+})
+
+testStream.once('end', () => {
+	if (summary.length > 0) {
+		console.error('--- ERRORS SUMMARY ---\n')
+		console.error(summary.join('\n'))
+		console.error('\n--- END OF SUMMARY ---')
+	}
+	process.exit(exitCode)
+})
