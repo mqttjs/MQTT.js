31cf07af45b5a4026bb929d8caa61bdc269a00c1
Daniel Lando <daniel.sorridi@gmail.com>
2023-10-09 18:02:02 +0200

    fix: eslint

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,5 +1,6 @@
 const isBrowser =
 	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
+	// eslint-disable-next-line no-restricted-globals
 	(typeof self !== 'undefined' && typeof self.postMessage === 'function') // is web worker
 
 export default isBrowser
