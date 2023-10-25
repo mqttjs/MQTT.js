130abb4c445bdccc175f29122a20fdbe5d0a5bbc
Horst Leung <horstleung@gmail.com>
2023-10-25 10:49:15 +0800

    Update is-browser.ts
    fix typo in comment

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -2,5 +2,5 @@ const isBrowser =
 	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
 	// eslint-disable-next-line no-restricted-globals
 	(typeof self !== 'undefined' && typeof self.postMessage === 'function') || // is web worker 
-  (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') // while navigator is deprecated
+  (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') // while navigator.product is deprecated
 export default isBrowser
