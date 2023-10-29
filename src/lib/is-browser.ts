16c429f20e64e9a7bbc09ffc239bb1c3a9b01334
Thomas Minitsios <thomas@minitsios.com>
2023-10-29 20:48:09 +0100

    fix: unambiguously detect web workers

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,6 +1,6 @@
 const isBrowser =
 	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
 	// eslint-disable-next-line no-restricted-globals
-	(typeof self !== 'undefined' && typeof self.postMessage === 'function') // is web worker
+	(typeof self === 'object' && self.constructor && self.constructor.name === 'DedicatedWorkerGlobalScope') // is web worker
 
 export default isBrowser
