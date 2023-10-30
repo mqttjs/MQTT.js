f6123f22a11a4eb4c34c874b47056cea7ef264a6
Horst Leung <horstleung@gmail.com>
2023-10-30 16:15:38 +0800

    fix: crash with React Native (#1724)

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,6 +1,6 @@
 const isBrowser =
 	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
 	// eslint-disable-next-line no-restricted-globals
-	(typeof self !== 'undefined' && typeof self.postMessage === 'function') // is web worker
-
+	(typeof self !== 'undefined' && typeof self.postMessage === 'function') || // is web worker
+	(typeof navigator !== 'undefined' && navigator.product === 'ReactNative') // while navigator.product is deprecated
 export default isBrowser
