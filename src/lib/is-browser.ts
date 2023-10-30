4556dc49d737bd58c19e502f344dbf6b96d4196b
Thomas Minitsios <thomas@minitsios.com>
2023-10-30 13:53:52 +0100

    fix linting issues

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,7 +1,11 @@
 const isBrowser =
 	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
 	// eslint-disable-next-line no-restricted-globals
-	(typeof self === 'object' && self.constructor && self.constructor.name === 'DedicatedWorkerGlobalScope') || // is web worker
+	(typeof self === 'object' &&
+		// eslint-disable-next-line no-restricted-globals
+		self.constructor &&
+		// eslint-disable-next-line no-restricted-globals
+		self.constructor.name === 'DedicatedWorkerGlobalScope') || // is web worker
 	(typeof navigator !== 'undefined' && navigator.product === 'ReactNative') // while navigator.product is deprecated
 
 export default isBrowser
