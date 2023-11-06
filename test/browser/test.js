9d8f12c4d530e4765b9ae89522ffc864ef7a1e73
Daniel Lando <daniel.sorridi@gmail.com>
2023-11-06 16:35:32 +0100

    fix: run tests both with ws/wss

--- test/browser/test.js
+++ test/browser/test.js
@@ -6,42 +6,48 @@ const mqtt = window.mqtt
 /** @type { import('@esm-bundle/chai').expect } */
 const expect = chai.expect;
 
-describe('MQTT.js browser test', () => {
-	const client = mqtt.connect('ws://localhost:4000', {
-		// log: console.log.bind(console),
-	})
-	client.on('offline', () => {
-		console.log('client offline')
-	})
-	client.on('connect', () => {
-		console.log('client connect')
-	})
-	client.on('reconnect', () => {
-		console.log('client reconnect')
-	})
-
-	it('should connect to the server', (done) => {
+function run(proto, port) {
+	describe('MQTT.js browser test with ' + proto.toUpperCase(), () => {
+		const client = mqtt.connect(`${proto}://localhost:${port}`, {
+			// log: console.log.bind(console),
+		})
+		client.on('offline', () => {
+			console.log('client offline')
+		})
 		client.on('connect', () => {
-			client.on('message', (topic, msg) => {
-				expect(topic).to.equal('hello');
-				expect(msg.toString()).to.equal('Hello World!');
-				client.end(() => {
-					done();
+			console.log('client connect')
+		})
+		client.on('reconnect', () => {
+			console.log('client reconnect')
+		})
+	
+		it('should connect-publish-subscribe', (done) => {
+			client.on('connect', () => {
+				client.on('message', (topic, msg) => {
+					expect(topic).to.equal('hello');
+					expect(msg.toString()).to.equal('Hello World!');
+					client.end(() => {
+						done();
+					});
+				});
+	
+				client.subscribe('hello', (err) => {
+					expect(err).to.not.exist;
+					if (!err) {
+						client.publish('hello', 'Hello World!', (err2) => {
+							expect(err2).to.not.exist;
+						});
+					}
 				});
 			});
-
-			client.subscribe('hello', (err) => {
-				expect(err).to.not.exist;
-				if (!err) {
-					client.publish('hello', 'Hello World!', (err2) => {
-						expect(err2).to.not.exist;
-					});
-				}
+	
+			client.on('error', (err) => {
+				done(err);
 			});
-		});
-
-		client.on('error', (err) => {
-			done(err);
-		});
+		})
 	})
-})
+}
+
+
+run('ws', 4000)
+run('wss', 4443)
