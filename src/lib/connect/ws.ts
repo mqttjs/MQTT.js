d0cdab8d62f5efbe00050939e39aa6ed5ea11257
Daniel Lando <daniel.sorridi@gmail.com>
2023-11-09 15:33:44 +0100

    style: add comment

--- src/lib/connect/ws.ts
+++ src/lib/connect/ws.ts
@@ -240,6 +240,7 @@ const browserStreamBuilder: StreamBuilder = (client, opts) => {
 
 	function onError(err: ErrorEvent) {
 		// fixes https://github.com/mqttjs/MQTT.js/issues/876
+		// this will be catched on client streamErrorHandler
 		stream.emit('error', err)
 		stream.destroy(err as any)
 	}
