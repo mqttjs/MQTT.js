b09414285d5c27cf76a9ff72cbb5ffe8ecec3981
hibrandonevans <112993120+hibrandonevans@users.noreply.github.com>
2023-11-22 15:26:56 +0800

    fix: improve environment detection in is-browser utility (#1744)

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,11 +1,18 @@
+const isStandardBrowserEnv = () =>
+	typeof window !== 'undefined' && typeof window.document !== 'undefined'
+
+const isWebWorkerEnv = () =>
+	Boolean(
+		// eslint-disable-next-line no-restricted-globals
+		typeof self === 'object' &&
+			// eslint-disable-next-line no-restricted-globals
+			self?.constructor?.name?.includes('WorkerGlobalScope'),
+	)
+
+const isReactNativeEnv = () =>
+	typeof navigator !== 'undefined' && navigator.product === 'ReactNative'
+
 const isBrowser =
-	(typeof window !== 'undefined' && typeof window.document !== 'undefined') ||
-	// eslint-disable-next-line no-restricted-globals
-	(typeof self === 'object' &&
-		// eslint-disable-next-line no-restricted-globals
-		self.constructor &&
-		// eslint-disable-next-line no-restricted-globals
-		self.constructor.name === 'DedicatedWorkerGlobalScope') || // is web worker
-	(typeof navigator !== 'undefined' && navigator.product === 'ReactNative') // while navigator.product is deprecated
+	isStandardBrowserEnv() || isWebWorkerEnv() || isReactNativeEnv()
 
 export default isBrowser
