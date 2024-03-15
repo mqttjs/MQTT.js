78e921320ddb2bce1691e8fcb12346d34f49e829
Daniel Lando <daniel.sorridi@gmail.com>
2024-03-15 12:04:09 +0100

    chore: ignore coverage of browser related stuff (#1819)
    * chore: ignore coverage of browser related stuff
    
    * fix: ignore type
    
    * fix: add missing lcov report

---
+++ nyc.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    include: [
+        'src/**',
+    ],
+    exclude: [
+        'src/bin/*',
+        'src/lib/BufferedDuplex.ts',
+        'src/connect/wx.ts',
+        'src/connect/ali.ts',
+    ],
+    reporter: [
+        'text',
+        'lcov',
+    ],
+    branches: 80,
+    functions: 89,
+    lines: 86,
+    statements: 86,
+    'check-coverage': true,
+}
