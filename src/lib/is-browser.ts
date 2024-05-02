3620992e90dcae0232dcf92e9892f9e602bb8225
axi92 <axi92@users.noreply.github.com>
2024-05-02 10:03:39 +0200

    fix(electron): improved code and add some comments

--- src/lib/is-browser.ts
+++ src/lib/is-browser.ts
@@ -1,16 +1,28 @@
 const isStandardBrowserEnv = () => {
+	// window is only defined when it is a browser
 	if (typeof window !== 'undefined') {
-		if (
-			navigator.userAgent.toLowerCase().indexOf(' electron/') > -1 ||
-			Object.prototype.hasOwnProperty.call(process.versions, 'electron')
-		) {
+		// Is the process an electron application
+		const electronMainCheck = Object.prototype.hasOwnProperty.call(
+			process.versions,
+			'electron',
+		)
+		// In case of electron the userAgent contains a string formated like: 'Electron/<version>'
+		// we can search for that to detect if it is an electron application
+		const electronRenderCheck =
+			navigator.userAgent.toLowerCase().indexOf(' electron/') > -1
+		if (electronMainCheck && electronRenderCheck) {
+			// Both electron checks are only true if the following webPreferences are set in the main electron BrowserWindow()
+			//   webPreferences: {
+			//     sandbox: false,
+			//     nodeIntegration: true
+			//     contextIsolation: false
+			// }
 			return false
 		}
-		return (
-			typeof window !== 'undefined' &&
-			typeof window.document !== 'undefined'
-		)
+		return typeof window.document !== 'undefined'
 	}
+	// return false if nothing is detected
+	return false
 }
 
 const isWebWorkerEnv = () =>
