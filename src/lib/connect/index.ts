d1a16531738b69f724e6acbd39533183b2360fb6
Daniel Lando <daniel.sorridi@gmail.com>
2024-08-08 14:48:12 +0200

    feat: add `quic` support
    Ref: #1298
    Fixes #1924

--- src/lib/connect/index.ts
+++ src/lib/connect/index.ts
@@ -109,7 +109,11 @@ function connect(
 
 	if (opts.cert && opts.key) {
 		if (opts.protocol) {
-			if (['mqtts', 'wss', 'wxs', 'alis'].indexOf(opts.protocol) === -1) {
+			if (
+				['mqtts', 'wss', 'wxs', 'alis', 'quic'].indexOf(
+					opts.protocol,
+				) === -1
+			) {
 				switch (opts.protocol) {
 					case 'mqtt':
 						opts.protocol = 'mqtts'
@@ -147,6 +151,8 @@ function connect(
 			protocols.ssl = require('./tls').default
 			protocols.tls = protocols.ssl
 			protocols.mqtts = require('./tls').default
+
+			protocols.quic = require('./quic').default
 		} else {
 			protocols.ws = require('./ws').browserStreamBuilder
 			protocols.wss = require('./ws').browserStreamBuilder
