3f0cdddcecd29864ab1ca764ea5402d29c992720
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-02-16 15:36:46 -0400

    fix(electron-test): test can not initialize

--- electron-test/wdio.conf.ts
+++ electron-test/wdio.conf.ts
@@ -1,22 +1,16 @@
-/// <reference types="wdio-electron-service" />
-import type { Options } from '@wdio/types'
+import { resolve as pathResolve } from 'node:path';
 import ServerLauncher from './test/service/server_launcher'
 
+const electronAppBinaryPath = pathResolve('./out/electron-test-linux-x64/electron-test');
 
-export const config: Options.Testrunner = {
+export const config: WebdriverIO.Config = {
     //
     // ====================
     // Runner Configuration
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
-    autoCompileOpts: {
-        autoCompile: true,
-        tsNodeOpts: {
-            project: './tsconfig.json',
-            transpileOnly: true
-        }
-    },
+    tsConfigPath: './tsconfig.json',
 
     //
     // ==================
@@ -67,6 +61,7 @@ export const config: Options.Testrunner = {
         // Electron service options
         // see https://webdriver.io/docs/desktop-testing/electron/configuration/#service-options
         'wdio:electronServiceOptions': {
+            appBinaryPath: electronAppBinaryPath,
             // custom application args
             appArgs: [],
         }
@@ -84,7 +79,7 @@ export const config: Options.Testrunner = {
     // Set specific log levels per logger
     // loggers:
     // - webdriver, webdriverio
-    // - @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
+    // - @wdio/browserstack-service, @wdio/lighthouse-service, @wdio/sauce-service
     // - @wdio/mocha-framework, @wdio/jasmine-framework
     // - @wdio/local-runner
     // - @wdio/sumologic-reporter
